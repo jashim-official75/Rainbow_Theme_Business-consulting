1db97c4a14a9265232439b778c28d2c693f7ea32
jashim-official75 <jashimuddinweb75@gmail.com>
2024-11-15 11:31:42 +0600

    this is final version

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,162 @@
 
 
 // ==========================
+// GSAP MARQUEE ANIMATION
+// ==========================
+
+gsap.registerPlugin(ScrollTrigger);
+
+let marqueeSpeed = 40; 
+let marqueeTween = gsap.to(".marquee__container-content ", {
+  x: `-=${marqueeSpeed}%`,
+  duration: 10,
+  ease: "none",
+  repeat: -1
+});
+
+ScrollTrigger.create({
+  trigger: ".marquee__container",
+  start: "10% bottom",
+  end: "bottom top",
+  onUpdate: (self) => {
+    marqueeTween.timeScale(self.direction === -1 ? -1 : 1);
+  }
+});
+
+
+ // ==========================
+// VIDEO POPUP
+// ==========================
+
+$(document).ready(function() {
+
+
+  function openPopup() {
+    $("#videoPopup").css("display", "flex");
+    $("body").css("overflow", "hidden");
+    const videoId = "DR9lxZ8kPYQ"; 
+    const videoUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+    $("#videoIframe").attr("src", videoUrl);
+  }
+
+  $(".playButtonMobile").on("click", function() {
+    openPopup();
+  });
+
+  $(".playButtonDesktop").on("click", function() {
+    openPopup();
+  });
+
+  $("#closePopup").on("click", closePopup);
+
+ 
+  $("#videoPopup").on("click", function(event) {
+    if (event.target.id === "videoPopup") {
+      closePopup();
+    }
+  });
+
+  function closePopup() {
+    $("#videoPopup").css("display", "none");
+    $("body").css("overflow", "auto");
+    $("#videoIframe").attr("src", ""); 
+  }
+});
+
+
+
+  
+// ======================================
+//AOS NEXT ANIMATION ACTIVATION CODE
+// ======================================
+
+AOS.init();
+
+        // ALL GSAP ANIMATION
+
+        var tl = gsap.timeline()
+
+        tl.from(".header__logo", {
+            y: -30,
+            duration: 1,
+            opacity: 0,
+            ease: "power2.out",
+        }, "0")
+
+        tl.from(".hero__title", {
+            y: -100,
+            duration: 1.5,
+            ease: "power2.out",
+            opacity: 0,
+            delay: 0.2,
+        }, 0);
+
+
+        tl.from(".nav__menu li , .primary-btn", {
+            y: -30,
+            duration: 1,
+            ease: "power2.out",
+            stagger: 0.15,
+            opacity: 0,
+            delay: 0.2,
+        })
+
+        gsap.to(".circle", {
+            rotation: 360,
+            repeat: -1,
+            ease: "linear",
+            duration: 10,
+        })
+
+        gsap.to(".dancer", {
+            y: -50,
+            rotation: 20,
+            duration: 3,
+            yoyo: true,
+            repeat: -1,
+            ease: "power1.inOut",
+        });
+
+        gsap.from(".fadein", {
+            x: 20,
+            opacity: 0,
+            duration: 2,
+
+        })
+
+        gsap.from(".fadeleft", {
+            x: -100,
+            opacity: 0,
+            duration: 2,
+
+        })
+
+        const waveHeading = document.querySelector(".hero__description");
+        waveHeading.innerHTML = waveHeading.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+        gsap.fromTo(".letter", {
+            y: 50,
+            opacity: 0
+        }, {
+            y: 0,
+            opacity: 1,
+            stagger: 0.05,
+            ease: "back.out(1)",
+            duration: 0.5
+        });
+
+// ==========================
 // MOBILE SIDEBAR
 // ==========================
 
 document.addEventListener('DOMContentLoaded', function() {
 
-  const menuIcon = document.querySelector('.menu__icon');
+  const menuIconDesktop = document.querySelector('.menu-icon');
+  const menuIconMobile = document.querySelector('.menu__icon');
   const sidebar = document.querySelector('.mobile_sidebar');
   const overlay = document.querySelector('.overlay');
   const closeButton = document.querySelector('.menu__close-btn');
-  const menuItems = document.querySelectorAll('.mobile_sidebar .mobile__menu'); 
-
+  const menuItems = document.querySelectorAll('.mobile_sidebar .mobile__menu');
 
   function openSidebar() {
     sidebar.classList.add('active');
@@ -19,17 +164,16 @@ document.addEventListener('DOMContentLoaded', function() {
     document.body.style.overflow = 'hidden';
   }
 
-
   function closeSidebar() {
     sidebar.classList.remove('active');
     overlay.style.display = 'none';
     document.body.style.overflow = 'auto';
   }
 
-  menuIcon.addEventListener('click', openSidebar);
+  if (menuIconDesktop) menuIconDesktop.addEventListener('click', openSidebar);
+  if (menuIconMobile) menuIconMobile.addEventListener('click', openSidebar);
   closeButton.addEventListener('click', closeSidebar);
   overlay.addEventListener('click', closeSidebar);
-
 
   menuItems.forEach(item => {
     item.addEventListener('click', closeSidebar);
@@ -125,8 +269,12 @@ var mySwiper = new Swiper('.our__work', {
     prevEl: '.work__arrow .swiper-button-prev',
   },
   breakpoints: {
-    1200: {
+    1366: {
       slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 2,
       spaceBetween: 20,
     },
 
@@ -249,67 +397,3 @@ $(document).ready(function() {
     },
   });
 });
-
-// ==========================
-// GSAP MARQUEE ANIMATION
-// ==========================
-
-gsap.registerPlugin(ScrollTrigger);
-
-let marqueeSpeed = 40; 
-let marqueeTween = gsap.to(".marquee__container-content ", {
-  x: `-=${marqueeSpeed}%`,
-  duration: 10,
-  ease: "none",
-  repeat: -1
-});
-
-ScrollTrigger.create({
-  trigger: ".marquee__container",
-  start: "10% bottom",
-  end: "bottom top",
-  onUpdate: (self) => {
-    marqueeTween.timeScale(self.direction === -1 ? -1 : 1);
-  }
-});
-
-
- // ==========================
-// VIDEO POPUP
-// ==========================
-
-$(document).ready(function() {
-
-
-  function openPopup() {
-    $("#videoPopup").css("display", "flex");
-    $("body").css("overflow", "hidden");
-    const videoId = "DR9lxZ8kPYQ"; 
-    const videoUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
-    $("#videoIframe").attr("src", videoUrl);
-  }
-
-  $(".playButtonMobile").on("click", function() {
-    openPopup();
-  });
-
-  $(".playButtonDesktop").on("click", function() {
-    openPopup();
-  });
-
-  $("#closePopup").on("click", closePopup);
-
- 
-  $("#videoPopup").on("click", function(event) {
-    if (event.target.id === "videoPopup") {
-      closePopup();
-    }
-  });
-
-  function closePopup() {
-    $("#videoPopup").css("display", "none");
-    $("body").css("overflow", "auto");
-    $("#videoIframe").attr("src", ""); 
-  }
-});
-
